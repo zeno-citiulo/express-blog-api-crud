2191b9967efa2991c0985cde6b96fb4514a709fa
zeno-citiulo <zenocit02@gmail.com>
2025-01-09 18:06:43 +0100

    feat: api crud error

--- src/server.js
+++ src/server.js
@@ -9,9 +9,12 @@ app.use(express.json());
 
 
 const postsRouter = require("./routes/posts");
-
-
 app.use("/posts", postsRouter);
+
+
+app.get("/", (req, res) => {
+  res.send("Benvenuto!");
+});
 
 
 app.get("/", (req, res) => {
@@ -21,4 +24,16 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server in esecuzione su http://localhost:${PORT}`);
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Endpoint non trovato" });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  const statusCode = err.status || 500;
+  const message = err.message || "Errore interno del server";
+  res.status(statusCode).json({ error: message });
 });
